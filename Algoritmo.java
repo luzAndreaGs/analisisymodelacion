44ef9ff1527f4be0c99bedf2179f90ee06206032
luzAndreaGs <123406054+luzAndreaGs@users.noreply.github.com>
2024-04-18 21:39:47 -0600

    Algoritmo.java

--- Algoritmo.java
+++ Algoritmo.java
@@ -1,17 +1,22 @@
-package algoritmo;
+package optimización;
 
 public class Algoritmo {
+    
 
     public static void main(String[] args) {
-        //Archivos a= new Archivos();
-       
+        // Obtener el tiempo inicial
+        long startTime = System.nanoTime();
+        Archivos a= new Archivos();  
+
+        String s1= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Escherichia coli strain.txt");  
+        String s2= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Enterococcus faecalis strain.txt");
+        String s3= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Porphyromonas gingivalis strain.txt");
+        String s4= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Helicobacter pylori isolate.txt");  
+        String s5= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Staphylococcus aureus strain.txt");
+        String s6= a.leertxt("C:\\Users\\Luz Andrea\\Documentos escuela\\Analisis y Modelacion\\Bacteroides fragilis strain.txt");
         
-        // String s1= a.leertxt("C:\\Users\\abere\\OneDrive\\Documentos\\Fasta\\monkeyPoxVirus.fasta");  
-        // String s2= a.leertxt("C:\\Users\\abere\\OneDrive\\Documentos\\Fasta\\Rhabdoviridae.fasta");
-        // String s3= a.leertxt("C:\\Users\\abere\\OneDrive\\Documentos\\Fasta\\coronaVirus.fasta");
-        
-        String[] secuencias={"e-jem-plo","per-ro-","g-ato-","-mur-ciela-go"};
-        //String[] secuencias={s1, s2, s3};
+//        String[] secuencias = {"paleontologia","biotecnologia","constitucionalidad","descomposicion"};
+        String[] secuencias={s1, s2, s3};
         Generacion obj = new Generacion(secuencias);
         obj.Ordenar();
         for (Individuo x : obj.poblacion) {
@@ -21,7 +26,7 @@ public class Algoritmo {
                 }
                 System.out.println("");
             }
-            System.out.println(x.Calificar()+"\n");
+            System.out.println(x.Calificar());
         }
         for (int z = 0; z < 10; z++){
             System.out.println("╔══════════════════╗");
@@ -32,12 +37,20 @@ public class Algoritmo {
             for (Individuo x : obj.poblacion) {
                 for (char[] y : x.secuencias) {
                     for (int i = 0; i < y.length; i++) {
-                        System.out.print(y[i]+" ");
+//                        System.out.print(y[i]+" ");
                     }
-                    System.out.println("");
+//                   System.out.println("");
                 }
-                System.out.println(x.Calificar()+"\n");
+                System.out.println(x.Calificar());
+                System.out.println(obj.verificarSecuencia(x)+"\n");
             }
         }
+        // Obtener el tiempo final
+        long endTime = System.nanoTime();
+        
+        // Calcular el tiempo transcurrido en segundos
+        double elapsedTimeSeconds = (endTime - startTime) / 1e9;
+        
+        System.out.println("Tiempo " + elapsedTimeSeconds);    
     }
 }
